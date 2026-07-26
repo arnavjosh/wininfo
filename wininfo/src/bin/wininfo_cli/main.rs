d1eded0449180eaf7193c90d748b1ba4ce85d50b
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2026-07-26 12:39:07 -0400

    Used clap for CLI

--- wininfo/src/bin/wininfo_cli/main.rs
+++ wininfo/src/bin/wininfo_cli/main.rs
@@ -8,14 +8,45 @@
     )
 )]
 
+#[cfg(feature = "cli")]
+use clap::{Parser, Subcommand};
 use std::error::Error;
 use wmr::System;
 
+#[cfg(feature = "cli")]
+#[derive(Parser)]
+#[command(about = "wininfo_cli - Windows system information tool", long_about = None)]
+#[command(version)]
+// The \x1B[4m and \x1B[0m are ANSI escape codes to underline the text and to reset formatting
+#[command(after_help = "\x1B[4mExamples:\x1B[0m\n\
+    # Print memory information\n\
+    wininfo_cli memory\n")]
+struct Cli {
+    // Then subcommands
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[cfg(feature = "cli")]
+#[derive(Subcommand)]
+enum Commands {
+    Memory {},
+    Cpu {},
+    Disks {},
+}
+
+#[cfg(feature = "cli")]
 fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+
     let system = System::new()?;
 
-    let info = system.info()?;
-
-    println!("{:}", serde_json::to_string_pretty(&info)?);
+    let out = match &cli.command {
+        Some(Commands::Cpu {}) => serde_json::to_string_pretty(&system.cpu()?),
+        Some(Commands::Memory {}) => serde_json::to_string_pretty(&system.memory()?),
+        Some(Commands::Disks {}) => serde_json::to_string_pretty(&system.disk()?),
+        None => serde_json::to_string_pretty(&system.info()?),
+    }?;
+    println!("{:}", out);
     Ok(())
 }
