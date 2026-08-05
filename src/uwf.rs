19adc02319ef88316f17633e10559e18035d79d2
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2026-08-04 21:29:40 -0400

    UWF struct

---
+++ src/uwf.rs
@@ -0,0 +1,28 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UWFInfo {
+    enabled: bool,
+    total_size: Option<Byte>,
+    used_size: Option<Byte>,
+}
+
+impl UWFInfo {
+    pub(crate) fn new(enabled: bool, total_size: Option<Byte>, used_size: Option<Byte>) -> Self {
+        Self {
+            enabled,
+            total_size,
+            used_size,
+        }
+    }
+
+    pub fn enabled(&self) -> &bool {
+        &self.enabled
+    }
+    pub fn total_size(&self) -> Option<Byte> {
+        &self.total_size
+    }
+    pub fn used_size(&self) -> Option<Byte> {
+        self.used_size
+    }
+}
